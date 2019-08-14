80214710c059e1523c36db909157b1293a1ac425
jeef <saimaiaz@hotmail.com>
2019-08-14 08:40:40 +0700

    'init_app'

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Schd1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                SomeMethod();
+                Thread.Sleep(1000*60*60);
+            }
+        }
+
+
+
+        static void SomeMethod()
+        {
+            Console.WriteLine("SomeMethod is called.");
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://dmama.pwa.co.th/mis/note.xml", "note.xml");
+            }
+
+        }
+    }
+}
